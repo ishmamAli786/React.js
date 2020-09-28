24136f9f5376f2f0fb28e80467be86920dab1a8f
Ishmam Ali <56753433+ishmamAli786@users.noreply.github.com>
2020-09-28 09:41:17 -0700

    material ui and chart.js

--- App.js
+++ App.js
@@ -1,32 +1,51 @@
-import React,{useEffect,useState} from 'react';
-function App(){
-  const [repos,setRepos]=useState([{}]);
-  useEffect(()=>{
-    async function getRepos(){
-     const response =await fetch("https://api.github.com/users/ishmamAli786/repos");
-    const data=await response.json();
-   //  console.log(data)
-    setRepos(data);
-   }
-   getRepos();
-  //   fetch('https://jsonplaceholder.typicode.com/posts/1')
-  // .then((response) => response.json())
-  // .then(json => {
-  //   setData(json);
-  // })
-  },[])
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import ButtonAppBar from './NavBar';
+import BarChart from './BarChart';
+const useStyles = makeStyles((theme) => ({
+  root: {
+    // backgroundColor: 'lightblue',
+    // '& > *': {
+    //   margin: theme.spacing(1),
+    //   width: theme.spacing(16),
+    //   // height: theme.spacing(16),
+    // },
+  },
+  paper: {
+    width: 400,
+    margin: '0 auto',
+    marginTop:50,
+    textAlign: 'center',
+    // padding:15,
+  },
+  input:{
+    width:'95%',
+  }
+}));
 
-return(
-  <div className="App">
-    <h1>You are seeing all repositories</h1>
-    <ul>
-      {repos.map((repoObj,ind)=>{
-        return(<li key={ind}>{repoObj.name}</li>)
-      })}
-    </ul>
-  </div>
-)
+
+
+function App() {
+  const classes = useStyles();
+  return (
+    <div>
+      {/* <ButtonAppBar/>
+      <Paper elevation={3} className={classes.paper}>
+        <h1>Login</h1>
+        <form className={classes.root} noValidate autoComplete="off">
+          <TextField label="Enter Email" className={classes.input}/>
+          <TextField label="Enter Password" className={classes.input} />
+          <Button variant="contained" color="primary" className={classes.input}>
+            LogIn
+                  </Button>
+        </form>
+      // </Paper> */}
+      <BarChart/>
+    </div>
+  )
 }
 
 export default App;
-
---
+++ BarChart.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {Bar} from 'react-chartjs-2';
+
+const data = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'My First dataset',
+      backgroundColor: 'rgba(255,99,132,0.2)',
+      borderColor: 'rgba(255,99,132,1)',
+      borderWidth: 1,
+      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+      hoverBorderColor: 'rgba(255,99,132,1)',
+      data: [5, 25, 50, 40, 80, 100, 40]
+    },
+    {
+        label: 'My Second dataset',
+        backgroundColor: 'rgba(25,85,80,0.2)',
+        borderColor: 'rgba(255,99,132,1)',
+        borderWidth: 1,
+        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+        hoverBorderColor: 'rgba(255,99,132,1)',
+        data: [5, 25, 50, 40, 80, 100, 40]
+      },
+  ]
+
+};
+
+// export default React.createClass({
+//   displayName: 'BarExample',
+function BarChart(){
+
+
+//   render() {
+    return (
+      <div>
+        <h2>Bar Example (custom size)</h2>
+        <Bar
+          data={data}
+          width={100}
+          height={50}
+          options={{
+            maintainAspectRatio: false
+          }}
+        />
+      </div>
+    );
+        // }
+}
+
+export default BarChart;
