56a80fcd4c0ca1ac35d0dfdd9875d19ef4e0bfd5
Ishmam Ali <56753433+ishmamAli786@users.noreply.github.com>
2020-10-09 10:18:52 +0500

    Fetch pokemon Api Using UseEffect and axios

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState} from 'react';
+import './App.css';
+import ComA from './ComA';
+function App(){
+  return (
+    <>
+    <ComA/>
+    </>
+  )
+}
+
+
+
+export default App;
---
+++ ComA.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+const ComA=()=>{
+    const [num,setNum]=useState();
+    const [name,setName]=useState();
+    const [moves,setMove]=useState();
+    useEffect(()=>{
+        async function getData(){
+            const res=await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`);
+            // console.log(res.data.name)
+            setName(res.data.name)
+            setMove(res.data.moves.length)
+        }
+        getData()
+    })
+    return(
+        <>
+        <h1>You Choose <span style={{color:'red'}}> {num}</span> value</h1>
+        <h1>My Name is <span style={{color:'red'}}> {name}</span> </h1>
+    <h1>i Have <span style={{color:'red'}}> {moves} </span></h1>
+        <select value={num} onChange={(event)=>{
+            setNum(event.target.value)
+        }}>
+            <option value="1">1</option>
+            <option value="25">25</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+        </select>
+        </>
+    )
+}
+export default ComA;
