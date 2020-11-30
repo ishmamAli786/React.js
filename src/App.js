7ca56640d88c583c891f441e691664fc08063000
ishmam ali <ishmamalikhan@gmail.com>
2020-11-30 16:07:04 +0500

    My Git Repo Fetch Through Api

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React,{useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [repos,setRepos]=useState([{}]);
-  useEffect(()=>{
+  const [repos, setRepos] = useState([{}]);
+  useEffect(() => {
 
-    async function fetchData(){
+    async function fetchData() {
       ///with Async await
       const response = await fetch('https://api.github.com/users/ishmamAli786/repos');
       const data = await response.json();
       console.log(data)
       setRepos(data)
     }
-  fetchData();
+    fetchData();
 
 
 
@@ -22,19 +22,19 @@ function App() {
     //   .then(json => {
     //     console.log(json)
     //     setData(json)
-      // })
+    // })
 
 
 
-  },[])
+  }, [])
   return (
     <div className="App">
-    <h1>Your Are Seeing All Repositories</h1>
-    <ul>
-    {repos.map((repo,index)=>{
-      return <li key={index}>{repo.name}</li>
-    })}
-    </ul>
+      <h1>Your Are Seeing All Repositories</h1>
+      <ul>
+        {repos.map((repo, index) => {
+          return <li key={index}>{repo.name}</li>
+        })}
+      </ul>
     </div>
   );
 }
