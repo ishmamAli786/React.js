b8a4811264faeed96b0e6bcb6fde7aff0f79da6a
ishmam ali <ishmamalikhan@gmail.com>
2020-12-20 21:36:33 +0500

    CRUD Operation With Redux Thunk

---
+++ src/components/Posts/Posts.js
@@ -0,0 +1,21 @@
+import React,{useEffect} from "react";
+import PostCard from "./PostCard";
+import { useSelector,useDispatch } from "react-redux";
+import {getPosts} from "../../actions/postAction"
+
+const Posts = () => {
+  const dispatch=useDispatch();
+  const posts = useSelector((state) => state.post.posts);
+  useEffect(()=>{
+    dispatch(getPosts());
+  },[])
+  return (
+    <div className="row">
+      {posts.map((postItem) => (
+        <PostCard postItem={postItem} />
+      ))}
+    </div>
+  );
+};
+
+export default Posts;
